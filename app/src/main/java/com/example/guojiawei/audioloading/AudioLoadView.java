a9beec19a8f2a176ebca485dd6bc05edbac2780d
guoGG <527740618@qq.com>
2017-03-04 18:26:40 +0800

    test
    test

---
+++ app/src/main/java/com/example/guojiawei/audioloading/AudioLoadView.java
@@ -0,0 +1,79 @@
+package com.example.guojiawei.audioloading;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by guojiawei on 2017/3/4.
+ */
+
+public class AudioLoadView extends View {
+
+    private int mViewWidth;
+    private int mViewHeight;
+    private Paint mAudioLinePaint;
+    private int mBeatValue = 0;
+
+    public AudioLoadView(Context context) {
+        super(context);
+        initPaint();
+    }
+
+    public AudioLoadView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPaint();
+
+    }
+
+    private void initPaint() {
+        mAudioLinePaint = new Paint();
+        mAudioLinePaint.setColor(Color.BLACK);
+        mAudioLinePaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w < h) {
+            w = h;
+        }
+        mViewWidth = w;
+        mViewHeight = h;
+
+        startAnim();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mAudioLinePaint.setStrokeWidth(mViewHeight / 8);
+
+        int startX = 0 + 10;
+
+        canvas.drawLine(startX, mViewHeight / 4 * 2 + mBeatValue, startX, mViewHeight, mAudioLinePaint);
+        canvas.drawLine(startX + mViewWidth / 4, mViewHeight / 4 * 3 - mBeatValue, startX + mViewWidth / 4, mViewHeight, mAudioLinePaint);
+        canvas.drawLine(startX + mViewWidth / 4 * 2, mViewHeight / 4 * 1 + mBeatValue, startX + mViewWidth / 4 * 2, mViewHeight, mAudioLinePaint);
+        canvas.drawLine(startX + mViewWidth / 4 * 3, mViewHeight / 4 * 3 - mBeatValue, startX + mViewWidth / 4 * 3, mViewHeight, mAudioLinePaint);
+    }
+
+    private void startAnim() {
+        ValueAnimator valueAnim = ValueAnimator.ofInt(mViewHeight / 4, 0, mViewHeight / 4);
+        valueAnim.setInterpolator(new LinearInterpolator());
+        valueAnim.setDuration(1000);
+        valueAnim.setRepeatCount(-1);
+        valueAnim.start();
+        valueAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mBeatValue = (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+    }
+
+}
